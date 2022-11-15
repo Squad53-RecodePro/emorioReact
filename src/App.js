56605c530c5f8c409225e1de15ab3f89dfffc0a3
nbraga <ncls.braga19@gmail.com>
2022-11-15 13:06:23 -0400

    teste

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Blog from "./pages/Blog";
 import Contato from "./pages/Contato";
 import Equipe from "./pages/Equipe";
 import Sobre from "./pages/Sobre";
-<<<<<<< HEAD
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -25,11 +24,6 @@ import "react-toastify/dist/ReactToastify.css";
 import { useAuth } from "./hooks/useAuth";
 
 import { Navigate } from "react-router-dom";
-=======
-import Artigo001 from "./pages/Artigos/Artigo001";
-import Artigo002 from "./pages/Artigos/Artigo002";
-import Artigo003 from "./pages/Artigos/Artigo003";
->>>>>>> 966e4a5a9c02e2a93116bcf7071cb8c3ab40017e
 
 const App = () => {
   const { auth, loading, user } = useAuth();
@@ -40,7 +34,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/blog" element={<Blog />} />
-<<<<<<< HEAD
         <Route path="/artigo1" element={<Artigo07 />} />
         <Route path="/artigo2" element={<Artigo06 />} />
         <Route path="/artigo3" element={<Artigo01 />} />
@@ -49,11 +42,6 @@ const App = () => {
         <Route path="/artigo6" element={<Artigo05 />} />
         <Route path="/artigo7" element={<Artigo04 />} />
 
-=======
-        <Route path="/Artigo001" element={< Artigo001 />} />
-        <Route path="/Artigo002" element={< Artigo002 />} />
-        <Route path="/Artigo003" element={< Artigo003 />} />
->>>>>>> 966e4a5a9c02e2a93116bcf7071cb8c3ab40017e
         <Route path="/contato" element={<Contato />} />
         <Route path="/equipe" element={<Equipe />} />
         <Route path="/sobre" element={<Sobre />} />
